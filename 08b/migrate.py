849ddc5422c5b7489eba65871459bf569abd42f2
victor-dat21b <vict702k@stud.kea.dk>
2024-05-06 16:42:29 +0200

    Made 08a and 08b

---
+++ 08b/migrate.py
@@ -0,0 +1,36 @@
+import mysql.connector
+from pymongo import MongoClient
+
+
+mysql_conn = mysql.connector.connect(
+    host="127.0.0.1",
+    port=3306, 
+    user="root",
+    password="12345",
+    database="electradrive"
+)
+
+
+mongo_client = MongoClient("mongodb://localhost:27017/")
+mongo_db = mongo_client["migrate_task"]
+mongo_collection = mongo_db["cars"]
+
+
+mysql_cursor = mysql_conn.cursor(dictionary=True)
+mysql_cursor.execute("SELECT * FROM customers")
+
+
+for row in mysql_cursor:
+    transformed_data = {
+        "customer_id": row["customer_id"],
+        "name": row["name"],
+        "email": row["email"],
+        "address": row["address"],
+        "car_id": row["car_id"],
+    }
+    mongo_collection.insert_one(transformed_data)
+
+mysql_cursor.close()
+mysql_conn.close()
+mongo_client.close()
+
